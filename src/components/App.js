838d395e40a0d0ce8d779fa9905a235d942eb609
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-10-24 21:34:50 -0230

    docker production file. incl nginx image

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  render() {
+    return <div>App3</div>;
+  }
+}
+
+App.propTypes = {};
+
+export default App;
