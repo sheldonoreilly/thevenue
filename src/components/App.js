88cdd423e69090dc64bfcb5a26c8efc221457e04
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-10-25 10:30:15 -0230

    testing github flow

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
-    return <div>kicking elasticbeanstalk in the clouded butt</div>;
+    return <div>testing github flow</div>;
   }
 }
 
